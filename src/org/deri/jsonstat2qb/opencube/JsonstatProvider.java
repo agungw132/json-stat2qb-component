715575682d76d7acf2ed10303ee3b2c2f79cf900
Arkadiusz Stasiewicz <arkadiusz.stasiewicz@deri.org>
2015-03-16 17:03:58 +0000

    basic configuration - dataset URL

--- src/org/deri/jsonstat2qb/opencube/JsonstatProvider.java
+++ src/org/deri/jsonstat2qb/opencube/JsonstatProvider.java
@@ -1,24 +1,38 @@
-package org.deri.jsonstat2qb.opencube;
+package org.deri.opencube.jsonstat2qb;
 
-public class JsonstatProvider extends AbstractFlexProvider<TarqlProvider.Config> {
+import java.io.Serializable;
+import java.io.StringReader;
+import java.util.Iterator;
+import java.util.List;
+
+import org.openrdf.model.Statement;
+
+import com.fluidops.iwb.model.ParameterConfigDoc;
+import com.fluidops.iwb.model.ParameterConfigDoc.Type;
+import com.fluidops.iwb.provider.AbstractFlexProvider;
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.Triple;
+
+public class JsonstatProvider extends AbstractFlexProvider<JsonstatProvider.Config> {
 	private static final long serialVersionUID = 1L;
 
 	public static class Config implements Serializable {
 
 		private static final long serialVersionUID = 1L;
 
-	}
-
-	@Override
-	public void gather(final List<Statement> res) throws Exception {
-
-		Config c = config;
+		@ParameterConfigDoc(desc = "URL of the input JSON-stat file. Use file:// URLs for local files.", type=Type.FILEEDITOR)
+		public String jsonFileLocation;
 
 	}
 
 	@Override
 	public Class<? extends Config> getConfigClass() {
 		return Config.class;
+	}
+
+	@Override
+	public void gather(List<Statement> res) throws Exception {
+		
 	}
 
 }
