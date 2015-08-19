d9bc5aa5dbbeecf120d23939378431c6bb116e38
Arkadiusz Stasiewicz <arkadiusz.stasiewicz@deri.org>
2015-08-19 16:27:49 +0100

    base: gather

--- src/org/deri/jsonstat2qb/opencube/JsonstatProvider.java
+++ src/org/deri/jsonstat2qb/opencube/JsonstatProvider.java
@@ -5,15 +5,22 @@ import java.io.StringReader;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.deri.jsonstat2qb.opencube.ui.EncodingSelectValueFactory;
+import org.deri.jsonstat2qb.jsonstat2qb;
 
 import org.openrdf.model.Statement;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.ValueFactoryImpl;
 
 import com.fluidops.iwb.model.ParameterConfigDoc;
 import com.fluidops.iwb.model.ParameterConfigDoc.Type;
 import com.fluidops.iwb.provider.AbstractFlexProvider;
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 public class JsonstatProvider extends AbstractFlexProvider<JsonstatProvider.Config> {
 	private static final long serialVersionUID = 1L;
@@ -51,7 +58,91 @@ public class JsonstatProvider extends AbstractFlexProvider<JsonstatProvider.Conf
 
 	@Override
 	public void gather(List<Statement> res) throws Exception {
+
+		Config c = config;
+
+		jsonstat2qb converter = new jsonstat2qb(null);
+
+		Model model = null;
+
+		// BaseUri
+		if (StringUtils.isNotBlank(c.systemBaseURI) ) {
+			converter.setBaseUri(c.systemBaseURI);
+		}
+
+		// Encoding
+		if (StringUtils.isNotBlank(c.encoding) && ( ! c.encoding.equals("Autodetect")) ) {
+			converter.setEncoding(c.encoding);
+		}
 		
+		try {
+
+			// jsonFileLocation
+			if (StringUtils.isNotBlank(c.jsonFileLocation) ) {
+				// TODO separate function
+				model = converter.jsonstat2qb(c.jsonFileLocation);
+				
+				StmtIterator iter = model.listStatements();
+
+				ValueFactory factory = new ValueFactoryImpl();
+				
+				int counter = 1;
+				 
+				while (iter.hasNext()) {
+					
+					Triple t = iter.nextStatement().asTriple();
+
+					Node n = t.getObject();
+					Statement st;
+
+					if (t.getSubject().isURI()) {
+						if (n.isLiteral()) {
+							// TODO Literals are treated as strings (no datatypes
+							// retained)
+							st = factory.createStatement(
+									factory.createURI(t.getSubject().getURI()),
+									factory.createURI(t.getPredicate().getURI()),
+									factory.createLiteral(n.toString()));
+						} else if (n.isURI()) {
+							st = factory.createStatement(
+									factory.createURI(t.getSubject().getURI()),
+									factory.createURI(t.getPredicate().getURI()),
+									factory.createURI(n.toString()));
+						} else {
+							st = factory.createStatement(
+									factory.createURI(t.getSubject().getURI()),
+									factory.createURI(t.getPredicate().getURI()),
+									factory.createBNode(n.toString()));
+						}
+					} else {
+						// subject is a blank node
+						if (n.isLiteral()) {
+							// TODO Literals are treated as strings (no datatypes retained)
+							st = factory.createStatement(
+									factory.createBNode(t.getSubject().toString()),
+									factory.createURI(t.getPredicate().getURI()),
+									factory.createLiteral(n.toString()));
+						} else if (n.isURI()) {
+							st = factory.createStatement(
+									factory.createBNode(t.getSubject().toString()),
+									factory.createURI(t.getPredicate().getURI()),
+									factory.createURI(n.toString()));
+						} else {
+							st = factory.createStatement(
+									factory.createBNode(t.getSubject().toString()),
+									factory.createURI(t.getPredicate().getURI()),
+									factory.createBNode(n.toString()));
+						}
+					}
+					res.add(st);
+				}
+
+			}
+			
+		} catch (Exception e) {
+			// don nothing
+		}
+
 	}
 
 }
